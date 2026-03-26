06a41059d3fd41831a8ef081211e6a15be583ee9
Mateus Henrique <mateuzhenriq3@gmail.com>
2026-03-25 23:56:02 -0300

    feat: adicionando funções na classe CONTA

--- src/project/mateuzin/banco/Conta.java
+++ src/project/mateuzin/banco/Conta.java
@@ -7,13 +7,30 @@ public class Conta {
     private double saldo;
 
     //Construtor definindo os valores
-
     public Conta(String titular, String senha, double saldo) {
         this.titular = titular;
         this.senha = senha;
         this.saldo = saldo;
     }
 
+    //Get para pegar o saldo
+    public double getSaldo(){
+        return this.saldo;
+    }
 
+    //Método de Saldo, adiciona dinheiro na conta
+    public double setSaldo(double valor){
+        this.saldo = this.saldo+valor;
+        return this.saldo;
+    }
 
+    //Método para sacar, tenta sacar o dinheiro
+    public double sacarSaldo (double valor){
+        //Se for verdade o valor ele saca, caso contrário ele não faz nada
+        if(this.saldo >= valor){
+            this.saldo = this.saldo - valor;
+            return this.saldo;
+        }
+        return valor;
+    }
 }
